8b6876638011a976d4e87987b3f2334a46d25b80
Miguel De La Cruz <iscrusman@gmail.com>
2018-09-19 12:46:44 -0700

    update
    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var routes = require("./routes/routes-babies-testrpc-uint.js");
-var routes = require("./routes/routes-babies.js");
+var routes = require("./routes/routes-babies-testrpc.js");
+// var routes = require("./routes/routes-babies.js");
 
 var app = express();
 
--- routes/routes-babies-testrpc.js
+++ routes/routes-babies-testrpc.js
@@ -1,10 +1,12 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-var contractAdd = '0x158368ba278b3dd25222c1437af30d2861626a8f';
-var CONTRACT = new web3.eth.Contract([{"constant":true,"inputs":[],"name":"getCommunityName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"listOfBabies","outputs":[{"name":"registeredName","type":"string"},{"name":"hashFingerprint","type":"string"},{"name":"mothersAddress","type":"address"},{"name":"hospitalAddress","type":"address"},{"name":"genero","type":"uint8"},{"name":"birthDay","type":"uint256"},{"name":"timeStamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_motherAddress","type":"address"}],"name":"getBabyByMotherAddress","outputs":[{"name":"registeredName","type":"string"},{"name":"hashFingerprint","type":"string"},{"name":"hospitalAddress","type":"address"},{"name":"genero","type":"uint8"},{"name":"birthDay","type":"uint256"},{"name":"timeStamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_communityName","type":"string"}],"name":"setCommunityName","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_registeredName","type":"string"},{"name":"_hashFingerprint","type":"string"},{"name":"_mothersAddress","type":"address"},{"name":"_hospitalAddress","type":"address"},{"name":"_genero","type":"uint8"},{"name":"_birthDay","type":"uint256"}],"name":"registerBaby","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_communityName","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"registeredName","type":"string"},{"indexed":false,"name":"timeStamp","type":"uint256"}],"name":"resultRegister","type":"event"}]);
+var contractAdd = '0xd9a9d8be9d21393f84e83e02cc11bb6705b8e988';
+var CONTRACT = new web3.eth.Contract([{"constant":false,"inputs":[{"name":"_registeredName","type":"string"},{"name":"_babyHashFingerprint","type":"uint256"},{"name":"_motherHashFingerprint","type":"uint256"},{"name":"_motherName","type":"string"},{"name":"_hospitalAddress","type":"address"},{"name":"_genero","type":"uint8"},{"name":"_birthDay","type":"uint256"}],"name":"registerBaby","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getCommunityName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"listOfBabies","outputs":[{"name":"registeredName","type":"string"},{"name":"motherHashFingerprint","type":"uint256"},{"name":"motherName","type":"string"},{"name":"hospitalAddress","type":"address"},{"name":"genero","type":"uint8"},{"name":"birthDay","type":"uint256"},{"name":"timeStamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_babyHashFingerprint","type":"uint256"}],"name":"getBabyByHashFingerprint","outputs":[{"name":"registeredName","type":"string"},{"name":"motherHashFingerprint","type":"uint256"},{"name":"motherName","type":"string"},{"name":"hospitalAddress","type":"address"},{"name":"genero","type":"uint8"},{"name":"birthDay","type":"uint256"},{"name":"timeStamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_communityName","type":"string"}],"name":"setCommunityName","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_communityName","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"registeredName","type":"string"},{"indexed":false,"name":"timeStamp","type":"uint256"}],"name":"resultRegister","type":"event"}]);
 CONTRACT.options.address = contractAdd;
-    
+
+var defaultAccount = '0x52018daae84f6194b25e2391c420dca0e9f71c7d';
+
 var appRouter = function (app) {
     
     // -----------------
@@ -21,7 +23,7 @@ var appRouter = function (app) {
                 users.push({
                     firstName: "Miguel Angel",
                     email: "miguel@email.com",
-                    defaultAccount: result[0],
+                    // defaultAccount: result[0],
                     id:num,
                     name: req.params.name
                 });
@@ -38,8 +40,6 @@ var appRouter = function (app) {
     // -----------------
     app.get("/API/babies/getCommunityName/", function(req, res) {
         var babies = [];
-        var defaultAccount = '0x6dc37363c52cbb0c8e8c30c3cd800884687b7e78';
-
         CONTRACT.methods.getCommunityName().call(
         {
             from: defaultAccount
@@ -63,7 +63,7 @@ var appRouter = function (app) {
     app.post("/API/babies/setCommunityName/", function(req, res) {
         var babies = [];
         var communityName = req.body.communityName;
-        var defaultAccount = '0x6dc37363c52cbb0c8e8c30c3cd800884687b7e78';
+
         CONTRACT.methods.setCommunityName(communityName).send(
         {
             from: defaultAccount,
@@ -90,16 +90,15 @@ var appRouter = function (app) {
     app.post("/API/babies/register/", function(req, res) {
         var babies = [];
         var strRegisteredName = req.body.registeredName;
-        var strHashFingerprint = req.body.hashFingerprint;
-        var addrMothersAddress = req.body.mothersAddress;
+        var uintBabyHashFingerprint = req.body.babyHashFingerprint;
+        var uintMotherHashFingerprint = req.body.motherHashFingerprint;
+        var strMotherName = req.body.motherName;
         var addrhospitalAddress = req.body.hospitalAddress;
         var uintGenero = req.body.genero;
         var uintBirthDay = req.body.birthDay;
                 
-        var defaultAccount = '0x8fbaaa61798c260aec0e50dbd6cbe6dc16f79501';
-
-        CONTRACT.methods.registerBaby(strRegisteredName, strHashFingerprint, addrMothersAddress, addrhospitalAddress,
-            uintGenero, uintBirthDay).send(
+        CONTRACT.methods.registerBaby(strRegisteredName, uintBabyHashFingerprint, uintMotherHashFingerprint,strMotherName,
+            addrhospitalAddress, uintGenero, uintBirthDay).send(
         {
             from: defaultAccount,
             gasLimit:3000000,
@@ -121,12 +120,40 @@ var appRouter = function (app) {
         });
     });
 
-    app.get("/API/babies/getBabyByMotherAddress/:address", function(req, res) {
+    app.post("/API/babies/registerV2/", function(req, res) {
         var babies = [];
-        var addressMother = req.params.address;
-        var defaultAccount = '0x8fbaaa61798c260aec0e50dbd6cbe6dc16f79501';
+        var strRegisteredName = req.body.registeredName;
+        var uintBabyHashFingerprint = req.body.babyHashFingerprint;
+        var uintMotherHashFingerprint = req.body.motherHashFingerprint;
+        var strMotherName = req.body.motherName;
+        
+        //News
+        var uintFatherHashFingerprint = req.body.fatherHashFingerprint;
+        var strFatherrName = req.body.fatherName;
 
-        CONTRACT.methods.getBabyByMotherAddress(addressMother).call(
+        var addrhospitalAddress = req.body.hospitalAddress;
+        var uintGenero = req.body.genero;
+        var uintBirthDay = req.body.birthDay;
+                
+        //News IMG:
+        //News
+         var strImgMother = req.body.imgMother;
+         var strFatherrName = req.body.imgFather; 
+
+        babies.push({
+            result: "OK",
+            tx: "0x095ff778fc19c1c9f5c7ecb213b4ea285cbf92dce25c8f89018f83276cae37a4"
+        });
+
+        res.status(200).send(babies);
+        
+    });
+
+    app.get("/API/babies/getBabyByHashFingerprint/:babyHashFingerprint", function(req, res) {
+        var babies = [];
+        var babyHashFingerprint = req.params.babyHashFingerprint;
+
+        CONTRACT.methods.getBabyByHashFingerprint(babyHashFingerprint).call(
         {
             from: defaultAccount
         }
@@ -136,7 +163,8 @@ var appRouter = function (app) {
                 babies.push({
                     result: "OK",
                     registeredName: result.registeredName,
-                    hashFingerprint: result.hashFingerprint,
+                    motherHashFingerprint: result.motherHashFingerprint,
+                    motherName: result.motherName,
                     hospitalAddress: result.hospitalAddress,
                     genero: result.genero,
                     birthDay: result.birthDay,
