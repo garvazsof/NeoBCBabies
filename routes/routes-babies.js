232e93d7b83812dbc4888b6e27bdc96dc77a5a93
Miguel De La Cruz <iscrusman@gmail.com>
2018-09-25 09:42:02 -0700

    getHistory
    history of transactions

--- routes/routes-babies.js
+++ routes/routes-babies.js
@@ -4,18 +4,42 @@ var router = express.Router();
 
 var ethers = require('ethers');
 var provider = ethers.providers.getDefaultProvider('ropsten');
+var etherscanProvider = new ethers.providers.EtherscanProvider("ropsten");
 
 //Ropsten Addres Contract (DLT Babies)
 var contractAddress  = '0x141a34c1f5751c878226a51645d34785797a6385';
 var abi =  [{"constant":true,"inputs":[],"name":"getCommunityName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_communityName","type":"string"}],"name":"setCommunityName","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_babyHashFingerprint","type":"bytes32"}],"name":"getBabyByHashFingerprint","outputs":[{"name":"_registeredName","type":"string"},{"name":"_genero","type":"uint256"},{"name":"_birthDay","type":"uint256"},{"name":"_timeStamp","type":"uint256"},{"name":"_data","type":"string"},{"name":"_hospitalAddress","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_registeredName","type":"string"},{"name":"_babyHashFingerprint","type":"bytes32"},{"name":"_genero","type":"uint256"},{"name":"_birthDay","type":"uint256"},{"name":"_data","type":"string"},{"name":"_hospitalAddress","type":"address"}],"name":"registerBaby","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_communityName","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"registeredName","type":"string"},{"indexed":false,"name":"timeStamp","type":"uint256"}],"name":"resultRegister","type":"event"}];
     
 // TODO:  Cuenta de pruebas
+// var addressAccount = '0x8e7eb9b1960abfed2e525932a03a15e3f72b8a02';
 var privateKey = '0x1AF0B7DD30D55DE69303C2DDCF7ABD654EA46AD44B50163307EF425CD8C316C5';
 
 //wallet para transacciones
 var wallet = new ethers.Wallet(privateKey,provider);
 var contract_sign = new ethers.Contract(contractAddress, abi, wallet);
 var contract = new ethers.Contract(contractAddress, abi, provider);
+
+
+router.get("/babies/transactions/:addressAccount", function(req, res) {
+    var babies = [];
+    var addressAccount = req.params.addressAccount;
+
+    etherscanProvider.getHistory(contractAddress).then(function(history) {
+        if(history.length > 0)
+        {
+            for(var i=0; i < history.length; i++){
+                if(history[i].from.toUpperCase() == addressAccount.toUpperCase()){
+                    babies.push({
+                        result: "OK",
+                        tx_hash: history[i].hash
+                    });
+                }            
+            }
+        }
+        res.status(200).send(babies); 
+    });
+    
+});
 
 
 router.get("/babies/getCommunityName/", function(req, res) {
